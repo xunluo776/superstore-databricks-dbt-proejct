6cefd36129c9768ba1423c2d7ca7329953b01896
Addie Luo <xun079963@gmail.com>
2025-11-03 23:57:35 +0000

    silver done

--- models/silver/superstore.sql
+++ models/silver/superstore.sql
@@ -1,3 +1,7 @@
+{{ config(materialized='incremental', unique_key='order_id') }}
 select
 *
-from {{source('source', 'superstore')}}
+from {{source('bronze', 'superstore')}}
+{% if is_incremental() %}
+where date_added >=  (select max(date_added) from {{this}})
+{% endif %}
